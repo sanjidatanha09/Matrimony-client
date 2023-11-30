c59327330ee6bafcf44cbb378888afdd150844c5
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-11-30 19:26:26 +0600

    add

--- src/Home/Navvar.jsx
+++ src/Home/Navvar.jsx
@@ -17,7 +17,7 @@ const Navvar = () => {
 
 
 
-        <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] lg:hover:bg-[#66451c] lg:hover:text-white lg:hover:p-1 md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 
             <NavLink to="/">
 
@@ -26,22 +26,22 @@ const Navvar = () => {
             </NavLink></li>
 
 
-        <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <li className="block lg:hover:bg-[#66451c] lg:hover:text-white lg:hover:p-1 font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 
             <NavLink to="/biodata">BioDatas</NavLink></li>
 
-        <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <li className="block lg:hover:bg-[#66451c] lg:hover:text-white lg:hover:p-1 font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 
             <NavLink to="/detailsBio">Details Bio</NavLink></li>
 
 
-        <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <li className="block lg:hover:bg-[#66451c] lg:hover:text-white lg:hover:p-1 font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
             <NavLink to="/about">About Us</NavLink></li>
 
 
 
 
-        <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+        <li className="block lg:hover:bg-[#66451c] lg:hover:text-white lg:hover:p-1 font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 
 
             <NavLink to="/contact">Contact Us</NavLink></li>
@@ -50,7 +50,7 @@ const Navvar = () => {
 
         {
             user?.email ? <>
-                <li className="block font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                <li className="block lg:hover:bg-[#66451c] lg:hover:text-white lg:hover:p-1 font-bold py-2 px-3 text-gray-500 rounded hover:bg-[#66451c] md:hover:bg-transparent md:hover:text-[#66451c] md:p-0 md:dark:hover:text-[#66451c] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 
 
                     <NavLink to="dashboard">Dashboard</NavLink></li>
@@ -117,6 +117,7 @@ const Navvar = () => {
 
 
                         </div>
+                  
 
              
                            
@@ -129,8 +130,9 @@ const Navvar = () => {
                         className="items-center justify-between flex w-auto order-1"
                         id="navbar-sticky"
                     >
-                        <ul className="flex text-xs md:text-sm md:font-medium  border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row mt-0 border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            {navLinks}
+                        <ul className="flex text-xs md:text-sm md:font-medium  border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row mt-0 border-0 dark:bg-gray-800 md:dark:bg-gray-900  dark:border-gray-700">
+                            {navLinks} 
+                     
 
 
                         </ul>
